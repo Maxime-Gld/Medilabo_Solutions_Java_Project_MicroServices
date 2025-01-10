a81bfb42eb615682d79028e357ab0f9d93b5a4a2
Max-Dev <Maxime.Gdev@hotmail.com>
2025-01-10 16:17:58 +0100

    création d'un DTO pour les reponse de controleur (dto + erreur)

---
+++ patient/src/main/java/com/medilabo/solutions/patient/dto/ApiResponseDTO.java
@@ -0,0 +1,22 @@
+package com.medilabo.solutions.patient.dto;
+
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class ApiResponseDTO<T> {
+    
+    private T data;
+    private List<String> errors;
+
+    public ApiResponseDTO(T data) {
+        this.data = data;
+        this.errors = null;
+    }
+
+    public ApiResponseDTO(List<String> errors) {
+        this.errors = errors;
+        this.data = null;
+    }
+}
