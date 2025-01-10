b2a507977430b4bb30275cf2a64202758767ffab
Max-Dev <Maxime.Gdev@hotmail.com>
2025-01-10 16:17:21 +0100

    mise en place du dto patient

---
+++ patient/src/main/java/com/medilabo/solutions/patient/dto/PatientDTO.java
@@ -0,0 +1,21 @@
+package com.medilabo.solutions.patient.dto;
+
+import java.time.LocalDate;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PatientDTO {
+    
+    private Integer id;
+    private String name;
+    private String lastname;
+    private LocalDate birthdate;
+    private String gender;
+    private String adress;
+    private String phone;
+}
