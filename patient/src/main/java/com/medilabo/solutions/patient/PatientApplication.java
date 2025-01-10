2b1f66d750c204ee6a4c663838c365c5d3a4442f
Max-Dev <Maxime.Gdev@hotmail.com>
2025-01-10 13:38:48 +0100

    changement nom de fichier

--- patient/src/main/java/com/medilabo/solutions/patient/PatientApplication.java
+++ patient/src/main/java/com/medilabo/solutions/patient/PatientApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PatientDatabaseApplication {
+public class PatientApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PatientDatabaseApplication.class, args);
+		SpringApplication.run(PatientApplication.class, args);
 	}
 
 }
