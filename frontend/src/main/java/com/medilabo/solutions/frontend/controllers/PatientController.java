88731598e95cfe8b4f12d1fa9c27c0fde3d4a4a0
Max-Dev <Maxime.Gdev@hotmail.com>
2025-02-02 06:12:59 +0100

    utilisation de httpClient a la place de WebClient ou restTemplate

--- frontend/src/main/java/com/medilabo/solutions/frontend/controllers/PatientController.java
+++ frontend/src/main/java/com/medilabo/solutions/frontend/controllers/PatientController.java
@@ -1,66 +1,140 @@
 package com.medilabo.solutions.frontend.controllers;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.medilabo.solutions.frontend.config.Hmac;
 import com.medilabo.solutions.frontend.dto.PatientDTO;
 
 @Controller
 @RequestMapping("/patient")
 public class PatientController {
-    
+
+    private Hmac hmacService = new Hmac();
     private String urlBackend = "http://localhost:8881/patient/";
+
     @GetMapping("/all")
-    public String patients(Model model) {
+    public String patients(Model model) throws IOException, InterruptedException {
+        // générer un hmac pour communiquer avec l'API
+        String randomString = hmacService.generateRandomString();
+        String hmac = hmacService.generateHmac(randomString, "medilabo");
 
-        PatientDTO[] patientsArray = restTemplate.getForObject(urlBackend+"all", PatientDTO[].class);
-        List<PatientDTO> patients = List.of(patientsArray);
-        model.addAttribute("patients", patients);
+        // Effectuer une requete HttpClient GET vers l'API pour obtenir la liste des
+        // patients
+        HttpClient httpClient = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(urlBackend + "all"))
+                .header("hmac", hmac)
+                .header("randomString", randomString)
+                .GET()
+                .build();
 
+        // récupérer la liste des patients
+        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
+        // Vérifier si la requête est réussie
+        if (response.statusCode() == 200) {
+            // Convertir la réponse JSON en liste de PatientDTO
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.registerModule(new JavaTimeModule());
+            List<PatientDTO> patients = objectMapper.readValue(response.body(), new TypeReference<List<PatientDTO>>() {
+            });
+
+            // Ajouter la liste des patients au modèle
+            model.addAttribute("patients", patients);
+        }
         return "patients";
     }
 
-    @GetMapping("/add")
-    public String addPatient(Model model) {
-        return "addPatient";
-    }
-
     @GetMapping("/update/{id}")
-    public String updatePatient(Model model, Integer id) {
-        // Effectuer une requete GET vers l'API pour obtenir un patient
-        PatientDTO patient = restTemplate.getForObject(urlBackend+"update/"+id, PatientDTO.class);
-        model.addAttribute("patient", patient);
-        
+    public String updatePatient(Model model, @PathVariable Integer id) throws IOException, InterruptedException {
+        // Effectuer une requete httClient PUT vers l'API pour obtenir un patient
+        HttpClient httpClient = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(urlBackend + id))
+                .GET()
+                .build();
+
+        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
+        if (response.statusCode() == 200) {
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.registerModule(new JavaTimeModule());
+            PatientDTO patient = objectMapper.readValue(response.body(), PatientDTO.class);
+            model.addAttribute("patient", patient);
+        }
         return "updatePatient";
     }
 
-    @DeleteMapping("/delete/{id}")
-    public String deletePatient(Model model, Integer id) {
-        // Effectuer une requete DELETE vers l'API pour supprimer un patient
-        restTemplate.delete(urlBackend+"delete/"+id);
-        
+    @PostMapping("/delete/{id}")
+    public String deletePatient(Model model, @PathVariable Integer id) throws IOException, InterruptedException {
+        // Effectuer une requete httpCient DELETE vers l'API pour supprimer un patient
+        HttpClient httpClient = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(urlBackend + "delete/" + id))
+                .DELETE()
+                .build();
+
+        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println("Statut : " + response.statusCode());
+        System.out.println("Corps : " + response.body());
+
         return "redirect:/patient/all";
     }
-
-
 
     @PostMapping("/add")
-    public String addPatient(Model model, PatientDTO patient) {
+    public String addPatient(Model model, PatientDTO patient) throws IOException, InterruptedException {
         // Effectuer une requete POST vers l'API pour ajouter un patient
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        String json = objectMapper.writeValueAsString(patient);
+
         System.out.println("patient: " + patient);
-        restTemplate.postForEntity(urlBackend+"add", patient, PatientDTO.class);
-        
+        HttpClient httpClient = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(urlBackend + "add"))
+                .header("Content-Type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(json))
+                .build();
+
+        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
         return "redirect:/patient/all";
     }
 
+    @PostMapping("/update/{id}")
+    public String updatePatient(Model model, @PathVariable Integer id, PatientDTO patient) throws IOException, InterruptedException {
+        // Effectuer une requete httClient PUT vers l'API pour modifier un patient
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        String json = objectMapper.writeValueAsString(patient);
+        HttpClient httpClient = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(urlBackend + "update/" + id))
+                .header("Content-Type", "application/json")
+                .PUT(HttpRequest.BodyPublishers.ofString(json))
+                .build();
 
+        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
 
-
+        if (response.statusCode() == 200) {
+            return "redirect:/patient/all";
+        }
+        return "updatePatient";
+    }
 
 }
