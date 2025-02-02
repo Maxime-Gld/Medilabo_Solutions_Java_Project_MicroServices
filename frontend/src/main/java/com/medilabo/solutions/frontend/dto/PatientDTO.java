4eaa88cedc7af85e780c78abaf8fca445fe6fdb2
Max-Dev <Maxime.Gdev@hotmail.com>
2025-02-02 06:14:38 +0100

    mise en place du json format pour récupération de la date a partir des templates

--- frontend/src/main/java/com/medilabo/solutions/frontend/dto/PatientDTO.java
+++ frontend/src/main/java/com/medilabo/solutions/frontend/dto/PatientDTO.java
@@ -2,6 +2,8 @@ package com.medilabo.solutions.frontend.dto;
 
 import java.time.LocalDate;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,6 +16,7 @@ public class PatientDTO {
     private Integer id;
     private String name;
     private String lastname;
+    @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate birthdate;
     private String gender;
     private String adress;
