feea967e3d1ef28a30fa1a9ac578fc6f67626650
Max-Dev <Maxime.Gdev@hotmail.com>
2025-03-10 08:20:26 +0100

    entité et dto pour note

--- notes/src/main/java/com/medilabo/solutions/notes/entities/NoteEntity.java
+++ notes/src/main/java/com/medilabo/solutions/notes/entities/NoteEntity.java
@@ -3,9 +3,13 @@ package com.medilabo.solutions.notes.entities;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Document(collection = "notes")
 public class NoteEntity {
     
