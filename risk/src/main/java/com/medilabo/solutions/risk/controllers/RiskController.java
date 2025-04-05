5f5ed40f58314365ea16422a33a059d053b3ddd4
Max-Dev <Maxime.Gdev@hotmail.com>
2025-04-05 16:03:14 +0200

    création du controller pour backend risk

--- risk/src/main/java/com/medilabo/solutions/risk/controllers/RiskController.java
+++ risk/src/main/java/com/medilabo/solutions/risk/controllers/RiskController.java
@@ -1,17 +1,24 @@
 package com.medilabo.solutions.risk.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.medilabo.solutions.risk.enums.RiskConstant;
+import com.medilabo.solutions.risk.services.RiskService;
+
 @RestController
 @RequestMapping("/risk")
 public class RiskController {
     
+    @Autowired
+    private RiskService riskService;
 
     @GetMapping("/{id}")
-    public String getRisk(@PathVariable int id) {
-        return "Risk " + id;
+    public String getRisk(@PathVariable int id) throws IllegalArgumentException, Exception {
+        RiskConstant risk = riskService.evaluateRisk(id).orElseThrow(() -> new IllegalArgumentException("Patient with id " + id + " not found"));
+        return risk.name();
     }
 }
