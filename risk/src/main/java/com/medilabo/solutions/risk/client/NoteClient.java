e609fb2e73abc76560e235ade644fe44c3b7d0ff
Max-Dev <Maxime.Gdev@hotmail.com>
2025-04-05 16:02:29 +0200

    création des service de communications avec les autres backend

--- risk/src/main/java/com/medilabo/solutions/risk/client/NoteClient.java
+++ risk/src/main/java/com/medilabo/solutions/risk/client/NoteClient.java
@@ -5,25 +5,48 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.List;
 
 import org.springframework.stereotype.Service;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.medilabo.solutions.risk.config.Hmac;
+import com.medilabo.solutions.risk.dto.NoteDTO;
 
 @Service
 public class NoteClient {
     
+    private static final String SECRET_KEY = "medilabo";
     private final String GATEWAY_URL = "http://gatewayService:8881/";
-    private final String BACKEND_NOTE_URL = GATEWAY_URL + "note/";
+    private final String BACKEND_NOTE_URL = GATEWAY_URL + "notes/";
     private final HttpClient httpClient;
+    private final Hmac hmacService = new Hmac();
 
     public NoteClient() {
         this.httpClient = HttpClient.newHttpClient();
     }
 
-    public String getNotes(int id) throws IOException, InterruptedException {
+    public List<NoteDTO> getNotes(int id) throws IOException, InterruptedException {
+
+        // générer un hmac pour communiquer avec l'API
+        String randomString = hmacService.generateRandomString();
+        String hmac = hmacService.generateHmac(randomString, SECRET_KEY);
+
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(BACKEND_NOTE_URL + id))
+                .header("hmac", hmac)
+                .header("message", randomString)
                 .GET()
                 .build();
-        return httpClient.send(request, HttpResponse.BodyHandlers.ofString()).body();
+        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
+        if (response.statusCode() == 200) {
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.registerModule(new JavaTimeModule());
+            return objectMapper.readValue(response.body(), new TypeReference<List<NoteDTO>>() {});
+        }
+        return null;
     }
 }
--- risk/src/main/java/com/medilabo/solutions/risk/client/PatientClient.java
+++ risk/src/main/java/com/medilabo/solutions/risk/client/PatientClient.java
@@ -8,23 +8,45 @@ import java.net.http.HttpResponse;
 
 import org.springframework.stereotype.Service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.medilabo.solutions.risk.config.Hmac;
+import com.medilabo.solutions.risk.dto.PatientDTO;
+
 @Service
 public class PatientClient {
 
+    private static final String SECRET_KEY = "medilabo";
     private final String GATEWAY_URL = "http://gatewayService:8881/";
     private final String BACKEND_PATIENT_URL = GATEWAY_URL + "patient/";
     private final HttpClient httpClient;
+    private final Hmac hmacService = new Hmac();
 
     public PatientClient() {
         this.httpClient = HttpClient.newHttpClient();
     }
 
-    public String getPatient(int id) throws IOException, InterruptedException {
+    public PatientDTO getPatient(int id) throws IOException, InterruptedException {
+
+        // générer un hmac pour communiquer avec l'API
+        String randomString = hmacService.generateRandomString();
+        String hmac = hmacService.generateHmac(randomString, SECRET_KEY);
+        
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(BACKEND_PATIENT_URL + id))
+                .header("hmac", hmac)
+                .header("message", randomString)
                 .GET()
                 .build();
-        return httpClient.send(request, HttpResponse.BodyHandlers.ofString()).body();
+        
+        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
+        if (response.statusCode() == 200) {
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.registerModule(new JavaTimeModule());
+            return objectMapper.readValue(response.body(), PatientDTO.class);
+        }
+        return null;
     }
     
 }
