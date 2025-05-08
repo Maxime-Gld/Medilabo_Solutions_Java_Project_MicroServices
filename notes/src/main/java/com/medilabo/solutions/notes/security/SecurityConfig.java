9203824847338b6a9c30fb0425fd96f838e453a4
Max-Dev <Maxime.Gdev@hotmail.com>
2025-05-08 23:32:27 +0200

    suppression d'import inutile

--- notes/src/main/java/com/medilabo/solutions/notes/security/SecurityConfig.java
+++ notes/src/main/java/com/medilabo/solutions/notes/security/SecurityConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
--- risk/src/main/java/com/medilabo/solutions/risk/security/SecurityConfig.java
+++ risk/src/main/java/com/medilabo/solutions/risk/security/SecurityConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
