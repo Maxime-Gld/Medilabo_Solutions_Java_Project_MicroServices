007379740e18086075d0186bbf527beae2df76e4
Max-Dev <Maxime.Gdev@hotmail.com>
2025-08-28 19:00:40 +0200

    ajout de la méthode findNotesByPatId dans NoteRepository pour récupérer les projections de notes par identifiant de patient et suppression du boolean pour mise en place du green code

--- notes/src/main/java/com/medilabo/solutions/notes/repositories/NoteRepository.java
+++ notes/src/main/java/com/medilabo/solutions/notes/repositories/NoteRepository.java
@@ -3,14 +3,19 @@ package com.medilabo.solutions.notes.repositories;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import com.medilabo.solutions.notes.dto.projection.NoteProjection;
 import com.medilabo.solutions.notes.entities.NoteEntity;
 
 @Repository
 public interface NoteRepository extends MongoRepository<NoteEntity, String> {
-    
+
     List<NoteEntity> findByPatId(int patId);
+
     void deleteByPatId(int patId);
-    boolean existsByPatIdAndNote(int patId, String note);
+
+    List<NoteProjection> findNotesByPatId(@Param("patId") int patId);
+
 }
