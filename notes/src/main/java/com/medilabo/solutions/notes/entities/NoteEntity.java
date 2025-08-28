950381eef4650ee8689d6d3e23099983c2095382
Max-Dev <Maxime.Gdev@hotmail.com>
2025-08-28 18:59:52 +0200

    ajout du champ 'patientName' dans NoteEntity et greencode: ajout de l'index composé sur 'patId' et 'note' pour eviter 2 requete lors des doublons

--- notes/src/main/java/com/medilabo/solutions/notes/entities/NoteEntity.java
+++ notes/src/main/java/com/medilabo/solutions/notes/entities/NoteEntity.java
@@ -3,6 +3,7 @@ package com.medilabo.solutions.notes.entities;
 import java.util.Date;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.CompoundIndex;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import lombok.AllArgsConstructor;
@@ -13,11 +14,13 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Document(collection = "notes")
+@CompoundIndex(def = "{'patId': 1, 'note': 1}", unique = true)
 public class NoteEntity {
     
     @Id
     private String id;
     private int patId;
+    private String patientName;
     private String note;
     private Date createdAt = new Date();
 }
