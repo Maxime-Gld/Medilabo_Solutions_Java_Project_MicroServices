cff03f358b837d231efd98f72484bc0e0a8020e3
Max-Dev <Maxime.Gdev@hotmail.com>
2025-09-19 10:22:32 +0200

    ajout de codes couleur pour améliorer la lisibilité des messages dans le script de lancement

--- stack/run.sh
+++ stack/run.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
-echo "🚀 Lancement des services avec reconstruction..."
+# Codes couleur
+GREEN="\033[0;32m"
+YELLOW="\033[1;33m"
+BLUE="\033[0;34m"
+RESET="\033[0m"  # remet la couleur par défaut
+
+echo -e "${BLUE}🚀 Lancement des services avec reconstruction...${RESET}"
 docker-compose up --build &
 
-# Récupère le PID pour savoir quand c'est prêt
 COMPOSE_PID=$!
 
-# Attente que le frontend soit accessible
-echo "⏳ Attente que le frontend soit disponible..."
+echo -e "${YELLOW}⏳ Attente que le frontend soit disponible...${RESET}"
 until curl -s -o /dev/null -w "%{http_code}" http://localhost:8080 | grep -q "200\|302"; do
-  echo "⏳ En attente que le front réponde avec un code HTTP 200 ou 302..."
+  echo -e "${YELLOW}⏳ En attente que le front réponde avec un code HTTP 200 ou 302...${RESET}"
   sleep 3
 done
 
-echo "✅ Frontend prêt ! Accédez au site : http://localhost:8080"
+echo -e "${GREEN}✅ Frontend prêt ! Accédez au site : http://localhost:8080${RESET}"
 wait $COMPOSE_PID
